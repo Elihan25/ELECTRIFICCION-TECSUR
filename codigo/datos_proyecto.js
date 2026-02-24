b72e146c88b338a45c08863dd389c40d5ab8ec6e
Elihan25 <elihan.poma.p@uni.pe>
2026-02-24 10:47:39 -0500

    Add files via upload

---
+++ codigo/datos_proyecto.js
@@ -0,0 +1,70 @@
+const dP = {
+    "gantt": [
+        {
+            "id": "A1",
+            "name": "OBRAS PROVISIONALES Y PRELIMINARES",
+            "start": "2026-02-16",
+            "end": "2026-02-23",
+            "progress": 77.5,
+            "custom_class": "bar-real"
+        },
+        {
+            "id": "A2",
+            "name": "TRABAJOS SUBTERR\u00c1NEOS MT",
+            "start": "2026-02-16",
+            "end": "2026-02-23",
+            "progress": 100,
+            "custom_class": "bar-real"
+        },
+        {
+            "id": "A8",
+            "name": "MONTAJE DE POSTES - BAJA TENSI\u00d3N",
+            "start": "2026-02-16",
+            "end": "2026-02-23",
+            "progress": 72.26,
+            "custom_class": "bar-real"
+        }
+    ],
+    "resumen": [
+        {
+            "n": 1,
+            "rAcu": 2.46
+        },
+        {
+            "n": 2,
+            "rAcu": 5.66
+        },
+        {
+            "n": 3,
+            "rAcu": 11.79
+        },
+        {
+            "n": 4,
+            "rAcu": 26.91
+        },
+        {
+            "n": 5,
+            "rAcu": 26.91
+        },
+        {
+            "n": 6,
+            "rAcu": 26.91
+        },
+        {
+            "n": 7,
+            "rAcu": 26.91
+        },
+        {
+            "n": 8,
+            "rAcu": 26.91
+        },
+        {
+            "n": 9,
+            "rAcu": 26.91
+        },
+        {
+            "n": 10,
+            "rAcu": 26.91
+        }
+    ]
+};
